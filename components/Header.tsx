c19b60f91109f8caf3fb403c0db1d5e179c2ff93
woutervellekooppixel <info@woutervellekoop.nl>
2025-07-20 20:41:57 +0200

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -12,7 +12,7 @@ export default function Header() {
   const pathname = usePathname()
 
   const isHome = pathname === '/'
-  let suffix = 'PHOTO'
+  let suffix = 'DOWNLOAD'
   if (pathname.startsWith('/concerts')) suffix = 'CONCERTS'
   else if (pathname.startsWith('/events')) suffix = 'EVENTS'
   else if (pathname.startsWith('/misc')) suffix = 'MISC'
@@ -36,33 +36,14 @@ export default function Header() {
       </Link>
 
       <nav className="hidden sm:flex items-center space-x-6 text-sm text-black">
-        <div className="relative group">
-          <div className="font-medium hover:text-black cursor-pointer">Portfolio</div>
-          <div className="absolute left-0 top-full pt-1 z-50">
-            <div className="invisible opacity-0 group-hover:visible group-hover:opacity-100 transition-opacity duration-200 bg-white shadow-md rounded border border-gray-200 min-w-[140px] space-y-1 py-1 px-2">
-              <Link href="/portfolio/concerts" className="hover:text-gray-600">Concerts</Link>
-              <Link href="/portfolio/events" className="hover:text-gray-600">Events</Link>
-              <Link href="/portfolio/misc" className="hover:text-gray-600">Misc</Link>
-            </div>
-          </div>
-        </div>
 
-        <Link href="/about" className="hover:text-gray-600">About</Link> 
-        {/*  <Link href="/shop" className="hover:text-gray-600">Shop</Link> */}
+<Link href="https://www.wouter.photo" className="hover:text-gray-600">Portfolio</Link> 
+        <Link href="https://www.wouter.photo/about" className="hover:text-gray-600">About</Link> 
 
         <a href="https://instagram.com/woutervellekoop" target="_blank" className="hover:text-gray-600"><FaInstagram size={16} /></a>
         <a href="https://linkedin.com/in/woutervellekoop" target="_blank" className="hover:text-gray-600"><FaLinkedin size={16} /></a>
         <a href="mailto:hello@wouter.photo" className="hover:text-gray-600"><FaEnvelope size={16} /></a>
 
-        {/* 🛒 Winkelwagen */}
-        {/*  <button onClick={() => toggleCart(true)} className="relative hover:text-gray-600">
-          <ShoppingCart size={18} />
-          {itemCount > 0 && (
-            <span className="absolute -top-1 -right-2 bg-black text-white text-xs px-1 rounded-full">
-              {itemCount}
-            </span>
-          )}
-        </button> */}
       </nav>
 
       <div className="sm:hidden">
