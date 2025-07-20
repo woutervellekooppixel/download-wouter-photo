d907eac4d60fd1f3e0e7560ebeb88a016cecf3b1
woutervellekooppixel <info@woutervellekoop.nl>
2025-07-20 22:08:54 +0200

    e

--- middleware.ts
+++ middleware.ts
@@ -4,10 +4,14 @@ import type { NextRequest } from 'next/server';
 export function middleware(request: NextRequest) {
   const { pathname } = request.nextUrl;
 
-  // Alleen rootpagina redirecten
+  // Alleen exact '/' redirecten
   if (pathname === '/') {
     return NextResponse.redirect('https://wouter.photo');
   }
 
   return NextResponse.next();
 }
+
+export const config = {
+  matcher: ['/'], // middleware alleen op de rootpagina
+};
